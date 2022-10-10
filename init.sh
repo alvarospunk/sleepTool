2cbd8e1b185fc8b99b0d7d5d6216a28cf518fc18
Álvaro Garrido <alvarospunk@hotmail.com>
2022-10-10 22:40:38 +0200

    Loop video

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 shutdown -h +$1
-firefox -new-window https://www.youtube.com/watch?v=87T2iT2cjkM&list=UUnOAynBmYKA1neozHQNF0mA &
+firefox -new-window https://www.youtube.com/watch?v=87T2iT2cjkM&list=UUnOAynBmYKA1neozHQNF0mA?autoplay=1&loop=1 &
